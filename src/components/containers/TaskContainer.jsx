55279539f8b90b9f2852c452e706acb9b2b2fc37
saarmann <kaspar.saarmann@gmail.com>
2020-03-11 16:22:54 +0200

    Design and implement task table connect to database

--- src/components/containers/TaskContainer.jsx
+++ src/components/containers/TaskContainer.jsx
@@ -24,86 +24,74 @@ export default class TaskContainer extends React.Component {
         .then((response) => {
   
           this.setState({tasks: response.data}); 
-        //   console.log(this.state.tasks);
     
         }).catch((exception)=>{
           console.log(exception);
         });
     }
 
-    // addDescription() {
-    //     const description = this.setState({description: this.state})this.state.
-    // }
-
     render() {
 
         const {tasks} = this.state;
 
         return (
-                <div className="content-wrapper">
-                     <div className="content">
-                     <div className="row">
-                                            <div className="col-sm-12">
-
-                    <div className="card card-table-border-none" data-scroll-height="580">
-                        <div className="card-header justify-content-between ">
-                        <h2>New Customers</h2>
-                        <div>
-
-                            <div className="dropdown show d-inline-block widget-dropdown">
-                                <a className="dropdown-toggle icon-burger-mini" href="#" role="button" id="dropdown-customar" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
-                                </a>
-                                <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown-customar">
-                                    <li className="dropdown-item"><a href="#">Action</a></li>
-                                    <li className="dropdown-item"><a href="#">Another action</a></li>
-                                    <li className="dropdown-item"><a href="#">Something else here</a></li>
-                                </ul>
-                                </div>
-                        </div>
-                        </div>
-                        
-                        <div className="card-body pt-0">
-                            <table className="table ">
-                                <tbody>
+            <div className="content-wrapper">
+             <div className="content">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card card-default">
+  
+                            <div className="card-header card-header-border-bottom d-flex justify-content-between" id="recent-orders">
+                              <h2>Tasks</h2>
+                            </div>
+                            
+                            <div className="card-body">
+                           
+                                <table className="table">
                                     <tr>
-                                        <td>
-                                            <div className="media">
-                                                <div className="media-image mr-3 rounded-circle">
-                                                    <a href="profile.html"><img className="rounded-circle w-45" src="assets/img/user/u1.jpg" alt="customer image"/></a>
-                                                </div>
-                                                <div className="media-body align-self-center">
-                                                    <a href="profile.html"><h6 className="mt-0 text-dark font-weight-medium">Selena Wagner</h6></a>
-                                                <small>@selena.oi</small>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>2 Orders</td>
-                                        <td className="text-dark d-none d-md-block">$150</td>
+                                        <th>Date</th>
+                                        <th>Subject</th>
+                                        <th>Assigned by</th>
+                                        <th>Priority</th>
+                                        <th>Deadline</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
                                     </tr>
-                                    <tr>
-                                        <td>
-                                        <div className="media">
-                                            <div className="media-image mr-3 rounded-circle">
-                                                <a href="profile.html"><img className="rounded-circle w-45" src="assets/img/user/u2.jpg" alt="customer image"/></a>
-                                            </div>
-                                            <div className="media-body align-self-center">
-                                                <a href="profile.html"><h6 className="mt-0 text-dark font-weight-medium">Walter Reuter</h6></a>
-                                            <small>@walter.me</small>
-                                            </div>
-                                        </div>
-                                        </td>
-                                        <td>5 Orders</td>
-                                        <td className="text-dark d-none d-md-block">$200</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                                    {tasks.map((e) => (
+                                        <tbody>
 
+                                            <tr>
+                                            <td>{e.taskDate}</td>
+                                                <td>
+                                                        <div className="media-body align-self-center">
+                                                            <h6 className="mt-0 text-dark font-weight-medium">{e.taskDescription}</h6>
+                                                        <small>{e.customer.customerName}</small>
+                                                        </div>
+                                                </td>
+                                                <td>
+                                                    <div className="media-image mr-3 rounded-circle">
+                                                            <a href="profile.html"><img className="rounded-circle w-25" src="assets/img/user/u1.jpg" alt="customer image"/></a>
+                                                    </div>
+                                                </td>
+                                                <td>{e.priority == 1 ? <span className="badge badge-danger">High</span> : "" }</td>
+                                                <td>{e.deadline}</td>
+                                                <td>{e.taskStatus == 0 ? <span className="badge badge-success">Open</span> :<span className="badge badge-secondary">Closed</span> }</td>
+                                                <td>
+                                                    <button type="button" className="btn btn-outline-info mdi mdi-eye-outline btn-sm" ></button>
+                                                    <button type="button" className="btn btn-outline-success mdi mdi-check btn-sm ml-2"></button>
+                                                    <button type="button" className=" btn btn-outline-info mdi mdi-email-outline btn-sm ml-2"></button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    ))}
+                                </table>             
+                              </div>
+                             
+                            </div>                     
                     </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
+                </div>
+             </div>
+            </div>
 
                 // <div classNameName="content-wrapper">
                 //     <div classNameName="content">
--- src/components/containers/userInfo.jsx
+++ src/components/containers/userInfo.jsx
@@ -9,109 +9,59 @@ export default class UserInfo extends React.Component {
     render() {
         return (
 
-      <div className="content-wrapper">
-        <div className="content">							
-        <div className="bg-white border rounded">
-								<div className="row no-gutters">
-									<div className="col-lg-4 col-xl-3">
-										<div className="profile-content-left pt-5 pb-3 px-3 px-xl-5">
-											<div className="card text-center widget-profile px-0 border-0">
-												<div className="card-img mx-auto rounded-circle">
-													<img src="assets/img/user/u6.jpg" alt="user image"/>
-												</div>
-												<div className="card-body">
-													<h4 className="py-2 text-dark">Albrecht Straub</h4>
-													<p>CEO</p>
+          <div className="content-wrapper">
+          <div className="content">
+              <div className="row">
+                  <div className="col-12">
+                      <div className="card card-default">
 
-												</div>
-											</div>
-
-											<hr className="w-100"/>
-											<div className="contact-info pt-4">
-												<h5 className="text-dark mb-1">Contact Information</h5>
-												<p className="text-dark font-weight-medium pt-4 mb-2">Email address</p>
-												<p>Albrecht.straub@gmail.com</p>
-												<p className="text-dark font-weight-medium pt-4 mb-2">Phone Number</p>
-												<p>+99 9539 2641 31</p>
-											</div>
-										</div>
-									</div>
-									<div className="col-lg-8 col-xl-9">
-										<div className="profile-content-right py-5">
-											<ul className="nav nav-tabs px-3 px-xl-5 nav-style-border" id="myTab" role="tablist">
-												<li className="nav-item">
-													<a className="nav-link active" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
-												</li>
-											</ul>
-											<div className="tab-content px-3 px-xl-5" id="myTabContent">
-
-												<div className="tab-pane fade show active" id="settings" role="tabpanel" aria-labelledby="settings-tab">
-                          <div className="mt-5">
-                            <form>
-                              <div className="form-group row mb-6">
-                                <label for="coverImage" className="col-sm-4 col-lg-2 col-form-label">User Image</label>
-                                <div className="col-sm-8 col-lg-10">
-                                  <div className="custom-file mb-1">
-                                    <input type="file" className="custom-file-input" id="coverImage" required/>
-                                    <label className="custom-file-label" for="coverImage">Choose file...</label>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="row mb-2">
-                                <div className="col-lg-6">
-                                  <div className="form-group">
-                                    <label for="firstName">First name</label>
-                                    <input type="text" className="form-control" id="firstName" />
-                                  </div>
-                                </div>
-
-                                <div className="col-lg-6">
-                                  <div className="form-group">
-                                    <label for="lastName">Last name</label>
-                                    <input type="text" className="form-control" id="lastName" />
-                                  </div>
-                                </div>
-                              </div>
-
-                              <div className="row mb-2">
-                                <div className="col-lg-12">
-                                  <div className="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="email" className="form-control" id="email" />
-                                  </div>
-                                </div>
-                              </div>
-                              
-                              <div className="row mb-2">
-                                <div className="col-lg-6">
-                                  <div className="form-group">
-                                    <label for="firstName">Old password</label>
-                                    <input type="text" className="form-control" id="firstName" />
-                                  </div>
-                                </div>
-
-                                <div className="col-lg-6">
-                                  <div className="form-group">
-                                    <label for="lastName">New password</label>
-                                    <input type="text" className="form-control" id="lastName" />
-                                  </div>
-                                </div>
-                              </div>
-
-                              <div className="d-flex justify-content-end mt-5">
-                                <button type="submit" className="btn btn-primary mb-2 btn-pill">Update Profile</button>
-                              </div>
-
-                            </form>
+                          <div className="card-header card-header-border-bottom d-flex justify-content-between" id="recent-orders">
+    
+                            <h2>Tasks</h2>
                           </div>
-                        </div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-                </div>
-                </div>
+
+                          <div className="card-body">
+                           
+                            <table className="table">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <div className="media">
+                                                <div className="media-image mr-3 rounded-circle">
+                                                    <a href="profile.html"><img className="rounded-circle w-45" src="assets/img/user/u1.jpg" alt="customer image"/></a>
+                                                </div>
+                                                <div className="media-body align-self-center">
+                                                    <a href="profile.html"><h6 className="mt-0 text-dark font-weight-medium">Selena Wagner</h6></a>
+                                                <small>@selena.oi</small>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>2 Orders</td>
+                                        <td className="text-dark d-none d-md-block">$150</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                        <div className="media">
+                                            <div className="media-image mr-3 rounded-circle">
+                                                <a href="profile.html"><img className="rounded-circle w-45" src="assets/img/user/u2.jpg" alt="customer image"/></a>
+                                            </div>
+                                            <div className="media-body align-self-center">
+                                                <a href="profile.html"><h6 className="mt-0 text-dark font-weight-medium">Walter Reuter</h6></a>
+                                            <small>@walter.me</small>
+                                            </div>
+                                        </div>
+                                        </td>
+                                        <td>5 Orders</td>
+                                        <td className="text-dark d-none d-md-block">$200</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </div>
+                          </div>                     
+                  </div>
+              </div>
+          </div>
+      </div>
                 );
     }
 }

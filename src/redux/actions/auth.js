a37d76acced03c2bd380d5318a8e0f37e9d6cd68
saarmann <kaspar.saarmann@gmail.com>
2020-05-10 14:25:49 +0300

    install redux, react-redux set up types set up auth reducers

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT,
+} from './types';
---
+++ src/redux/actions/types.js
@@ -0,0 +1,35 @@
+// SignUp and Login
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+
+// Dashboard
+
+//Tasks
+export const GET_USER_TASKS = 'GET_USER_TASKS';
+export const CREATE_NEW_TASK = 'CREATE_NEW_TASK';
+export const GET_ALL_TASKS = 'GET_ALL_TASKS';
+export const UPDATE_TASK = 'UPDATE_TASKS';
+export const DELETE_TASK = 'DELETE_TASK';
+
+//Time
+export const GET_TIME_EXP = 'GET_TIME_EXP';
+export const UPDATE_TIME_EXP = 'UPDATE_TIME_EXP';
+export const DELETE_TIME_EXP = 'DELETE_TIME_EXP';
+export const INVOICE_TIME_EXP = 'INVOICE_TIME_EXP';
+
+//Customers
+export const GET_CUSTOMERS = 'GET_CUSTOMERS';
+export const CREATE_CUSTOMER = 'CREATE_CUSTOMER';
+export const UPDATE_CUSTOMER = 'UPDATE_CUSTOMER';
+
+//Invoices
+export const GET_INVOICES = 'GET_INVOICES';
+export const CREATE_INVOICE = 'CREATE_INVOICE';
+export const UPDATE_INVOICE = 'UPDATE_INVOICE';
+export const SEND_INVOICE = 'SEND_INVOICE';
+export const DELETE_INVOICE = 'DELETE_INVOICE';
